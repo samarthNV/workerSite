8ae50ff5c0e914f11ed3923eed34554846da8e74
Anonymous123-create <samarthvbura@gmail.com>
2023-07-30 01:49:58 +0530

    Updated Client Side

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
 const date = require(__dirname + "/date.js");
 
-const port_no = 27017;
+const port_no =  process.env.PORT || 27017;
 
 mongoose.connect("mongodb://127.0.0.1:27017/", {
     dbName: "WorkerConnect",
@@ -14,11 +15,28 @@ mongoose.connect("mongodb://127.0.0.1:27017/", {
 .catch((e) => {console.log(e)});
 
 const userSchema = new mongoose.Schema({
+    name: String,
     email: String,
     password: String,
 })
 
+const taskSchema = new mongoose.Schema({
+    task: String,
+    domain: String,
+    description: String,
+    todate: String,
+})
+
+const preTaskSchema = new mongoose.Schema({
+  task: String,
+  domain: String,
+  description: String,
+  todate: String,
+})
+
 const User = mongoose.model("User-info", userSchema);
+const Task = mongoose.model("Task-info", taskSchema);
+const PreTask = mongoose.model("PreTask-info", preTaskSchema);
 
 const app = express();
 
@@ -27,16 +45,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+let person = 0;
+
 const isAuthenticated = async (req, res, next) => {
-    const {token} = req.cookies;
-    if(token){
-        const decode = jwt.verify(token, "secretKey");
-        req.user = await User.findById(decoded._id);
+    const { token } = req.cookies;
+    if (token) {
+        const decoded = jwt.verify(token, "secretKey");
+        person = await User.findById(decoded._id);
         next();
-    }else{
-        res.render("login"); 
+    } else {
+      res.redirect("/login");
     }
-}
+  };
 
 app.get("/home", isAuthenticated, (req, res) => {
     res.render("clientHome");
@@ -59,37 +79,116 @@ app.get("/register", (req, res) => {
 })
 
 app.get("/profile", (req, res) => {
-    res.render("clientProfile");
+    res.render("clientProfile", {
+      name: person.name, 
+      email: person.email,
+    });
 })
 
-app.get("/notifications", (req, res) => {
-    res.render("clientNotify");
+app.get("/notifications", async (req, res) => {
+  let taskArray = await Task.find();
+    res.render("clientNotify", {
+      taskArray: taskArray,
+      message: "Your task was successfully uploaded !",
+    });
 })
 
-app.get("/tasks", (req, res) => {
-    res.render("clientTasks");
+app.get("/tasks", async (req, res) => {
+    let taskArray = await Task.find();
+    let previousTask = await PreTask.find();
+    res.render("clientTasks", {
+        taskArray: taskArray,
+        previousTask: previousTask,
+    });
 })
 
 app.get("/addTask", (req, res) => {
     res.render("clientAddTask");
 })
 
-app.post("/home", async (req, res) => {
-    const {email, password} = req.body;
-    const user = await User.create({
-        email,
-        password,
-    });
-    const token = jwt.sign({_id: user._id}, "secretKey");
-
+app.post("/login", async (req, res) => {
+    const { emaill, passwordl } = req.body;
+  
+    let user = await User.findOne({email : emaill });
+  
+    if (!user) return res.redirect("/register");
+  
+    const isMatch = await bcrypt.compare(passwordl, user.password);
+  
+    if (!isMatch)
+      return res.render("login", { email : emaill, message: "Incorrect Password" });
+  
+    const token = jwt.sign({ _id: user._id }, "secretKey");
+  
     res.cookie("token", token, {
-        httpOnly:true,
-        expires: new Date(Date.now() + 40*1000),
-    })
+      httpOnly: true,
+      expires: new Date(Date.now() + 100 * 1000),
+    });
     res.redirect("/home");
+  });
+
+app.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+  
+    let user = await User.findOne({email : email });
+    if (user) {
+      return res.redirect("/login");
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+  
+    user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+  
+    const token = jwt.sign({ _id: user._id }, "secretKey");
+  
+    res.cookie("token", token, {
+      httpOnly: true,
+      expires: new Date(Date.now() + 100 * 1000),
+    });
+    res.redirect("/home");
+});
+
+app.post("/tasks", async (req, res) => {
+    const {task, domain, description} = req.body;
+    let textdate = new Date(Date.now());
+    let todate = textdate.toString();
+    todate = todate.substring(4, 24);
+    console.log(todate);
+
+    task = await Task.create({
+        task,
+        domain,
+        description,
+        todate,
+    });
+
+    res.redirect("/tasks");
+
 })
 
-// app.post("/login", async)
+app.post("/taskPrev", async (req, res) => {
+  let mark = req.body.mark;
+  let prtask = await Task.findOne({task : mark});
+  preTask = await PreTask.insertMany(prtask);
+  task = await Task.deleteOne({task : mark});
+  res.redirect("/tasks");
+})
+
+app.get("/task/:taskNames", function(req, res){
+    let taskName = _.lowerCase(req.params.taskNames);
+    tasks.forEach(function(element){
+      let bTitle = _.lowerCase(element.title);
+      if(bTitle === postName){
+        res.render("post", {
+          blogTitle: element.title,
+          blogContent: element.content,
+        })
+      }
+    })
+  })
 
 app.listen( port_no, () => {
     console.log("Server has started on PORT 27017");
